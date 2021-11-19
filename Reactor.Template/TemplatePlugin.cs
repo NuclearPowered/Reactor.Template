e606b05bf4fcd9dbd78ba5f37eabd64032e0cc19
js6pak <kubastaron@hotmail.com>
2021-11-19 22:32:03 +0100

    Simplify

--- Reactor.Template/TemplatePlugin.cs
+++ Reactor.Template/TemplatePlugin.cs
@@ -4,31 +4,30 @@ using BepInEx.IL2CPP;
 using HarmonyLib;
 using Reactor;
 
-namespace Reactor.Template
+namespace Reactor.Template;
+
+[BepInAutoPlugin]
+[BepInProcess("Among Us.exe")]
+[BepInDependency(ReactorPlugin.Id)]
+public partial class TemplatePlugin : BasePlugin
 {
-    [BepInAutoPlugin("me.change.please")]
-    [BepInProcess("Among Us.exe")]
-    [BepInDependency(ReactorPlugin.Id)]
-    public partial class TemplatePlugin : BasePlugin
+    public Harmony Harmony { get; } = new(Id);
+
+    public ConfigEntry<string> ConfigName { get; private set; }
+
+    public override void Load()
     {
-        public Harmony Harmony { get; } = new Harmony(Id);
+        ConfigName = Config.Bind("Fake", "Name", ":>");
 
-        public ConfigEntry<string> ConfigName { get; private set; }
+        Harmony.PatchAll();
+    }
 
-        public override void Load()
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.FixedUpdate))]
+    public static class ExamplePatch
+    {
+        public static void Postfix(PlayerControl __instance)
         {
-            ConfigName = Config.Bind("Fake", "Name", ":>");
-
-            Harmony.PatchAll();
-        }
-
-        [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.FixedUpdate))]
-        public static class ExamplePatch
-        {
-            public static void Postfix(PlayerControl __instance)
-            {
-                __instance.nameText.text = PluginSingleton<TemplatePlugin>.Instance.ConfigName.Value;
-            }
+            __instance.nameText.text = PluginSingleton<TemplatePlugin>.Instance.ConfigName.Value;
         }
     }
 }
